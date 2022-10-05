8a2fb1bf8e6366abdeb2bf5f038968af8beb3af1
Joey Ballentine <joeyjballentine@gmail.com>
2022-10-05 00:08:08 -0400

    new way of getting python lib

--- setup.py
+++ setup.py
@@ -7,6 +7,71 @@ import sys
 from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
 import sysconfig
+import itertools
+
+
+def get_python_library(python_version):
+    """Get path to the python library associated with the current python
+    interpreter."""
+    # determine direct path to libpython
+    python_library = sysconfig.get_config_var("LIBRARY")
+
+    # if static (or nonexistent), try to find a suitable dynamic libpython
+    if python_library is None or os.path.splitext(python_library)[1][-2:] == ".a":
+
+        candidate_lib_prefixes = ["", "lib"]
+
+        candidate_extensions = [".lib", ".so", ".a"]
+        if sysconfig.get_config_var("WITH_DYLD"):
+            candidate_extensions.insert(0, ".dylib")
+
+        candidate_versions = [python_version]
+        if python_version:
+            candidate_versions.append("")
+            candidate_versions.insert(0, "".join(python_version.split(".")[:2]))
+
+        abiflags = getattr(sys, "abiflags", "")
+        candidate_abiflags = [abiflags]
+        if abiflags:
+            candidate_abiflags.append("")
+
+        # Ensure the value injected by virtualenv is
+        # returned on windows.
+        # Because calling `sysconfig.get_config_var('multiarchsubdir')`
+        # returns an empty string on Linux, `du_sysconfig` is only used to
+        # get the value of `LIBDIR`.
+        libdir = sysconfig.get_config_var("LIBDIR")
+        if sysconfig.get_config_var("MULTIARCH"):
+            masd = sysconfig.get_config_var("multiarchsubdir")
+            if masd:
+                if masd.startswith(os.sep):
+                    masd = masd[len(os.sep) :]
+                libdir = os.path.join(libdir, masd)
+
+        if libdir is None:
+            libdir = os.path.abspath(
+                os.path.join(sysconfig.get_config_var("LIBDEST"), "..", "libs")
+            )
+
+        candidates = (
+            os.path.join(libdir, "".join((pre, "python", ver, abi, ext)))
+            for (pre, ext, ver, abi) in itertools.product(
+                candidate_lib_prefixes,
+                candidate_extensions,
+                candidate_versions,
+                candidate_abiflags,
+            )
+        )
+
+        for candidate in candidates:
+            if os.path.exists(candidate):
+                # we found a (likely alternate) libpython
+                python_library = candidate
+                break
+
+    # TODO(opadron): what happens if we don't find a libpython?
+
+    return python_library
 
 
 class CMakeExtension(Extension):
@@ -31,7 +96,9 @@ class CMakeBuild(build_ext):
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE={}".format(extdir),
             "-DPYTHON_EXECUTABLE={}".format(sys.executable),
             "-DPYTHON_INCLUDE_DIR={}".format(sysconfig.get_path("include")),
-            "-DPYTHON_LIBRARY={}".format(sysconfig.get_config_var("LIBDIR")),
+            "-DPYTHON_LIBRARY={}".format(
+                get_python_library(sysconfig.get_python_version())
+            ),
             "-DCMAKE_BUILD_TYPE={}".format(cfg),
             "-DCMAKE_CROSSCOMPILING=ON",
             "-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64",
